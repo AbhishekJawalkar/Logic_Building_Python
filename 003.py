cef41bc5825a340a0332a1182eb10fac72bb81f8
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 14:25:26 +0530

    Question 3

---
+++ 003.py
@@ -0,0 +1,9 @@
+# Swap the values of two variables without using a third (temporary) variable.
+
+a = 10
+b = 20
+print("Before Swapping")
+print(a , b)
+a , b = b , a
+print("After Swapping")
+print(a , b)
