f98c2e120499063be33093fa5696389c8d960aa9
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 14:26:59 +0530

    Question 13

---
+++ 013.py
@@ -0,0 +1,8 @@
+# Given two numbers, use comparison operators to print whether the first is greater, smaller, or equal — all using boolean expressions, without an if statement.
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+print("First Number is greater : ", num1 > num2)
+print("First Number is smaller : ", num1 < num2)
+print("Both are Equal : ", num1 == num2)
