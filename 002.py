4030beaf29f918a6a50cd027c26a2f011728b282
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 14:25:14 +0530

    Question 2

---
+++ 002.py
@@ -0,0 +1,10 @@
+# Take two numbers as input and display their sum, difference, product, quotient, and remainder.
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+print(f"Sum = {num1} + {num2} = {num1 + num2}")
+print(f"Difference = {num1} - {num2} = {num1 - num2}")
+print(f"Product = {num1} * {num2} = {num1 * num2}")
+print(f"Quotient = {num1} / {num2} = {num1 / num2}")
+print(f"Remainder = {num1} % {num2} = {num1 % num2}")
