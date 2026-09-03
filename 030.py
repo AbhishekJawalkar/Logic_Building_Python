2b0de5642fae94d0dcebb8dec56ecbee5dd5bb0e
Abhishek Jawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 17:37:54 +0530

    Question 30

---
+++ 030.py
@@ -0,0 +1,19 @@
+# Check whether a given number is prime.
+
+num = int(input("Enter a Number : "))
+
+if num <= 1:
+    print("Not a Prime Number")
+
+else: 
+    is_prime = True
+
+    for i in range(2,num):
+        if num % i == 0:
+            is_prime = False
+            break
+        
+    if is_prime:
+        print("Prime Number")
+    else:
+        print("Not a Prime Number")
