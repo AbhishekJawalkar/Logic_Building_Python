a0271d273a10a42e996fd58060c451501cfa9dd8
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 14:27:06 +0530

    Question 14

---
+++ 014.py
@@ -0,0 +1,13 @@
+# Create five variables of different types (int, float, string, boolean, list) and print each one's type using
+
+Var1 = 91
+Var2 = 91.91
+Var3 = "\"91\""
+Var4 = True
+Var5 = [91]
+
+print(Var1, type(Var1))
+print(Var2, type(Var2))
+print(Var3, type(Var3))
+print(Var4, type(Var4))
+print(Var5, type(Var5))
