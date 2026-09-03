be031cc4c73742561a187dc15f232b751e70670a
Abhishek Jawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 16:00:10 +0530

    Question 22

---
+++ 022.py
@@ -0,0 +1,9 @@
+# Determine whether a person is eligible to vote based on their age.
+
+Age = int(input("Enter your age : "))
+
+if Age >= 18:
+    print("Eligible to Vote")
+else:
+    print("Not Eligible to Vote")
+    
