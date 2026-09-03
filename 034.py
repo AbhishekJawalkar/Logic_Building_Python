33f0fde51c483908b9f7eb8a9efe54d17649e9c3
Abhishek Jawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 23:31:28 +0530

    Question 34

---
+++ 034.py
@@ -0,0 +1,12 @@
+# Find the sum of digits of a given number.
+
+num = int(input("Enter a Number : "))
+
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + digit
+    num = num // 10
+
+print(sum)
