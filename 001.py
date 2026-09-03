e5b2cfd24008f00ec6503f770a02bf453e3b39db
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-09-03 14:24:33 +0530

    Question 1

---
+++ 001.py
@@ -0,0 +1,7 @@
+# Print your name, age, and city using three separate variables and an f-string.
+
+Name = "Abhishek Jawalkar"
+Age = 22
+City = "Pune"
+
+print(f"Hello Everyone, My name is {Name}, i am {Age} years old and I live in {City}")
