54f3bf1df7729f0e0d89da2f12698c551c67513e
Abhishek Jawalkar <abhishekjawalkar111@gmail.com>
2026-09-05 10:48:10 +0530

    Question 41

---
+++ 041.py
@@ -0,0 +1,10 @@
+# Write a function that takes two numbers and returns their sum (no print inside the function)
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+def Sum(num1,num2):
+    sum = num1 + num2
+    return sum
+
+print(Sum(num1, num2))
